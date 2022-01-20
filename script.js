2d12a649f75dfaabccdeb6ef60e2c723a62193fd
Michael Chong <mikec0920@gmail.com>
2022-01-19 16:21:29 -0800

    Initialize project

---
+++ script.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, numPages, read) {
+  this.title = title
+  this.author = author
+  this.numPages = numPages,
+  this.read = read
+  this.info = function() {
+    return title + ' by ' + author + ', ' + numPages + ' pages, ' + (read ? 'read' : 'not read yet')
+  }
+}
+
+function addBookToLibrary() {
+  let title = prompt("Enter the book title: ")
+  let author = prompt("Enter the book author: ")
+  let numPages = prompt("Enter the number of pages in the book: ")
+  let read = prompt("Has the book been read? ")
+  let newBook = new Book(title, author, numPages, read)
+  myLibrary.push(newBook)
+}
+
+addBookToLibrary()
+// const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true)
+// console.log(hobbit.info())
