ccd418949393bdbf07564eb1288f7218bdde0da1
Michael Chong <mikec0920@gmail.com>
2022-03-10 17:17:18 -0800

    Add books display functionality

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 let myLibrary = [];
 
 function Book(title, author, numPages, read) {
-  this.title = title
-  this.author = author
-  this.numPages = numPages,
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.read = read;
   this.info = function() {
     return title + ' by ' + author + ', ' + numPages + ' pages, ' + (read ? 'read' : 'not read yet')
   }
 }
 
 function addBookToLibrary() {
-  let title = prompt("Enter the book title: ")
-  let author = prompt("Enter the book author: ")
-  let numPages = prompt("Enter the number of pages in the book: ")
-  let read = prompt("Has the book been read? ")
-  let newBook = new Book(title, author, numPages, read)
-  myLibrary.push(newBook)
+  let title = prompt("Enter the book title: ");
+  let author = prompt("Enter the book author: ");
+  let numPages = prompt("Enter the number of pages in the book: ");
+  let read = prompt("Has the book been read? ");
+  let newBook = new Book(title, author, numPages, read);
+  myLibrary.push(newBook);
 }
 
-addBookToLibrary()
-// const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true)
-// console.log(hobbit.info())
+function displayLibrary() {
+  let list = document.querySelector("ul");
+  myLibrary.forEach(function(book) {
+    entry = document.createElement("li");
+    entry.innerHTML = book.info();
+    list.appendChild(entry);
+  });
+}
+
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+myLibrary.push(hobbit);
+displayLibrary();
