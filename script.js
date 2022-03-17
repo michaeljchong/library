9052a4b617e5b5aa104d7fe40c8f4fbbc6bb8426
Michael Chong <mikec0920@gmail.com>
2022-03-16 17:11:33 -0700

    Create dynamic form element to add new books to library

--- script.js
+++ script.js
@@ -9,31 +9,91 @@ function Book(title, author, numPages, read) {
     return title + ' by ' + author + ', ' + numPages + ' pages, ' + (read ? 'read' : 'not read yet')
   }
   this.changeRead = function() {
-    this.read = this.read ? false : true;
+    
   }
 }
 
-function addBookToLibrary() {
+function newBookForm() {
   let f = document.createElement("form");
   f.setAttribute('method', "post");
-  f.setAttribute('action', "submit.php");
+  f.setAttribute('action', "");
   let titleLabel = document.createElement("label");
   titleLabel.setAttribute('for', "title");
   titleLabel.innerHTML = "Enter the book title:";
   let title = document.createElement("input");
   title.setAttribute('type', "text");
   title.setAttribute('name', "title");
-  title.id = "title";
-  // let author = prompt("Enter the book author: ");
-  // let numPages = prompt("Enter the number of pages in the book: ");
-  // let read = prompt("Has the book been read? ");
+  title.required = true;
+  let authorLabel = document.createElement("label");
+  authorLabel.setAttribute('for', "author");
+  authorLabel.innerHTML = "Enter the book author:";
+  let author = document.createElement("input");
+  author.setAttribute('type', "text");
+  author.setAttribute('name', "author");
+  let numPagesLabel = document.createElement("label");
+  numPagesLabel.setAttribute('for', "numPages");
+  numPagesLabel.innerHTML = "Enter the number of pages in the book:";
+  let numPages = document.createElement("input");
+  numPages.setAttribute('type', "number");
+  numPages.setAttribute('name', "numPages");
+  let readLabel = document.createElement("label");
+  readLabel.setAttribute('for', "read");
+  readLabel.innerHTML = "Has the book been read?";
+  let wasReadDiv = document.createElement("div");
+  let wasRead = document.createElement("input");
+  wasRead.setAttribute('type', "radio");
+  wasRead.setAttribute('name', "read");
+  wasRead.setAttribute('value', true);
+  let wasReadLabel = document.createElement("label");
+  wasReadLabel.setAttribute('for', "Yes");
+  wasReadLabel.innerHTML = "Yes";
+  let unReadDiv = document.createElement("div");
+  let unRead = document.createElement("input");
+  unRead.setAttribute('type', "radio");
+  unRead.setAttribute('name', "read");
+  unRead.setAttribute('value', false);
+  let unReadLabel = document.createElement("label");
+  unReadLabel.setAttribute('for', "No");
+  unReadLabel.innerHTML = "No";
+  let submit = document.createElement("button");
+  submit.setAttribute('type', "button");
+  submit.className = "submit";
+  submit.innerHTML = "Submit";
+  wasReadDiv.appendChild(wasRead);
+  wasReadDiv.appendChild(wasReadLabel);
+  unReadDiv.appendChild(unRead);
+  unReadDiv.appendChild(unReadLabel);
+  f.appendChild(titleLabel);
+  f.appendChild(title);
+  f.appendChild(authorLabel);
+  f.appendChild(author);
+  f.appendChild(numPagesLabel);
+  f.appendChild(numPages);
+  f.appendChild(readLabel);
+  f.appendChild(wasReadDiv);
+  f.appendChild(unReadDiv);
+  f.appendChild(submit);
+  document.body.appendChild(f);
+  document.querySelector(".submit").addEventListener("click", addBookToLibrary);
+}
+
+function addBookToLibrary() {
+  let title = document.querySelector("input[name='title']").value;
+  let author = document.querySelector("input[name='author']").value;
+  let numPages = document.querySelector("input[name='numPages']").value;
+  let read = document.querySelector("input[value='true']").checked;
 
   let newBook = new Book(title, author, numPages, read);
   myLibrary.push(newBook);
+  document.querySelector("form").remove();
+  displayLibrary();
 }
+
+
 
 function displayLibrary() {
   let display = document.querySelector(".display");
+  display.innerHTML = null;
   for (let index = 0; index < myLibrary.length; index++) {
     let book = document.createElement("div");
     book.id = index;
@@ -48,7 +108,8 @@ function displayLibrary() {
 }
 
 let newBook = document.querySelector(".new");
-newBook.addEventListener("click", addBookToLibrary);
+newBook.addEventListener("click", newBookForm);
+
 
 const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
 myLibrary.push(hobbit);
